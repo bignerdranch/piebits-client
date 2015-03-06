6f49115ea160c8631c46511ec4894e488ae09d01
Brian Hardy <brian@bignerdranch.com>
2015-03-06 13:28:57 -0500

    Makes app submit to actual service.

--- lib/piebits/app.rb
+++ lib/piebits/app.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'faraday'
 
 module Piebits
   class App
@@ -7,11 +8,13 @@ module Piebits
     attr_reader :arguments
     
     DEFAULT_CONFIG_FILENAME = ".piebits.yml"
+    PIEBITS_URL = "http://piebits.com"
     
-    def initialize(environment:, arguments:, config_filename:DEFAULT_CONFIG_FILENAME, 
+    def initialize(environment:, arguments:, faraday:nil, config_filename:DEFAULT_CONFIG_FILENAME, 
         input_io:STDIN, output_io:STDOUT, error_io:STDERR)
       @environment = environment
       @working_dir = environment['PWD'] # TODO: allow override with an argument
+      @faraday = faraday || Faraday.new(PIEBITS_URL)
       @config_filename = config_filename
       @arguments = arguments
       @input_io = input_io
@@ -33,13 +36,16 @@ module Piebits
         )
       # generate a build
       build = build_generator.generate_build
-      # convert the build to JSON
-      json = JSON.generate(build.to_hash)
-      # spit it out
-      @output_io.puts json
-      
-      # upload the build to our service
-      
+      # submit the build to the service
+      submitter = BuildSubmitter.new(build: build, faraday: @faraday)
+      response = submitter.submit_build
+      if response.success?
+        @output_io.puts "Build submitted successfully."
+        return 0
+      else
+        @error_io.puts "Failed to submit build. Response: #{response}"
+        return 1
+      end
     ensure
       $stderr = previous_stderr
     end
--- spec/piebits/app_spec.rb
+++ spec/piebits/app_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'faraday'
 
 describe Piebits::App do
   
@@ -9,7 +10,7 @@ describe Piebits::App do
     expect(app.arguments).to eq(ARGV)
   end
   
-  it "can be run" do
+  it "can run and submit a build successfully" do
     fixture_dir = File.join(__dir__, "../fixtures")
     output_string_io = StringIO.new
     error_string_io = StringIO.new
@@ -17,13 +18,25 @@ describe Piebits::App do
       'PWD' => fixture_dir,
       'TRAVIS_COMMIT' => 'abc123'
     }
-    app = described_class.new(environment: env, arguments: [],
+    expected_json = "{\"timestamp\":null,\"commit_sha\":\"abc123\",\"ci_build_url\":null,\"reports\":[{\"category\":\"analysis\",\"tool_name\":\"oclint\",\"tool_version\":null,\"data\":\"fake oclint json\"}]}"
+
+    # stub out our expected request with a success response
+    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.post('/api/builds', expected_json, { 'Content-Type' => 'application/json' }) { |env| [ 200, {}, '' ] }
+    end
+    # create a test faraday using our stubs
+    test_faraday = Faraday.new do |builder|
+      builder.adapter :test, stubs
+    end
+    
+    app = described_class.new(environment: env, arguments: [], faraday: test_faraday,
       config_filename: 'config-with-oclint.yml', output_io: output_string_io, error_io: error_string_io)
-    app.run
+    exit_code = app.run
     
-    expected_output = "{\"timestamp\":null,\"commit_sha\":\"abc123\",\"ci_build_url\":null,\"reports\":[{\"category\":\"analysis\",\"tool_name\":\"oclint\",\"tool_version\":null,\"data\":\"fake oclint json\"}]}\n"
-    
-    expect(output_string_io.string).to eq(expected_output)
+    expect(exit_code).to eq(0)
+    expect(output_string_io.string).to eq("Build submitted successfully.\n")
     expect(error_string_io.string).to eq("")
+    # ensure that we got called
+    stubs.verify_stubbed_calls
   end
 end
